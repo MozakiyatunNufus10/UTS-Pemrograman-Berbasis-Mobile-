b176499885db50307695f66239cf01c301d029d5
MozakiyatunNufus10 <nufusmozakiyatun@gmail.com>
2026-05-07 22:27:39 +0700

    colors.js

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#2E7D32',
+  primaryDark: '#1B5E20',
+  primaryLight: '#E8F5E9',
+  background: '#F1F8E9',
+  white: '#FFFFFF',
+  danger: '#E53935',
+  textPrimary: '#222222',
+  textSecondary: '#888888',
+  border: '#E8F5E9',
+};
